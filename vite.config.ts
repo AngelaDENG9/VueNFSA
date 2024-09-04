d2e16d6b143a163e0b283a9a9809d6141a361241
AngelaDENG9 <u3243808@uni.canberra.edu.au>
2024-09-04 12:14:06 +1000

    up

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vueNFSA/',
+  // base: '/vueNFSA/',
   plugins: [
     vue(),
     vueJsx(),
